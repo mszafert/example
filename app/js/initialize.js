688b40fbe7e2259a3d8055c85e90b1896fca2888
Marcin Szafert <mszafert@pgs-software.com>
2017-12-18 10:36:33 +0100

    es6 refactor

--- app/js/initialize.js
+++ app/js/initialize.js
@@ -1,11 +1,19 @@
 import Skicams from './skicams';
 
-$(window).on('resize', function() {
-    var h = $(window).height() - $('header').outerHeight() - $('footer').outerHeight() - 100;
+new Skicams;
+
+$(window)
+  .on('resize', () => {
+    const h =
+      $(window).height() -
+      $('header').outerHeight() -
+      $('footer').outerHeight() -
+      100;
     $('.contact').height(h);
-}).trigger('resize');
+  })
+  .trigger('resize');
 
 $('.contact-form').validate({
-    debug: true,
-    errorClass: 'uk-form-danger'
+  debug: true,
+  errorClass: 'uk-form-danger',
 });
--- app/js/skicams.js
+++ app/js/skicams.js
@@ -1,74 +1,76 @@
-var Skicams = function() {};
+class Skicams {
+  constructor() {
+    this.result = [];
+    this.today = '';
+    this.div = $('#skicams-container') || null;
+    this.getData();
+    this.getCurrentDate();
+  }
 
-Skicams.prototype = {
-    _div: null,
-    _result: [],
-    _today: '',
-    init: function() {
-        this._div = $('#skicams-container');
-        this.getData();
-        this.getCurrentDate();
-    },
-    getData: function() {
-        $.ajax({
-            url: 'https://makevoid-skicams.p.mashape.com/cams.json',
-            type: 'GET',
-            dataType: 'json',
-            success: function(data) {
-                this.processData(data);
-            }.bind(this),
-            error: function(err) {
-                alert(err);
-            },
-            beforeSend: function(xhr) {
-                xhr.setRequestHeader("X-Mashape-Authorization", "kxSXmUymofmshFHhhKxWOSJpqJsJp1I3zNnjsnqKwhITAiC1zw");
-            }
-        });
-    },
-    processData: function(data) {
-        for(var i in data) {
-            this._result[data[i].name] = data[i].cams;
-        }
-        // debugger;
-        this.displayResult();
+  getData() {
+    $.ajax({
+      url: 'https://makevoid-skicams.p.mashape.com/cams.json',
+      type: 'GET',
+      dataType: 'json',
+      success: function(data) {
+        this.processData(data);
+      }.bind(this),
+      error(err) {
+        alert(err);
+      },
+      beforeSend(xhr) {
+        xhr.setRequestHeader(
+          'X-Mashape-Authorization',
+          'kxSXmUymofmshFHhhKxWOSJpqJsJp1I3zNnjsnqKwhITAiC1zw'
+        );
+      },
+    });
+  }
 
-    },
-    getCurrentDate: function() {
-        var today = new Date(),
-            dd = today.getDate(),
-            mm = today.getMonth() + 1,
-            yyyy = today.getFullYear();
+  processData(data) {
+    Object.keys(data).forEach(key => {
+      this.result[data[key].name] = data[key].cams;
+    });
+    this.displayResult();
+  }
 
-        if (dd < 10) { dd = '0' + dd; }
-        if (mm < 10) { mm = '0' + mm; }
+  getCurrentDate() {
+    const today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth() + 1;
+    const yyyy = today.getFullYear();
 
-        this._today = mm + '-' + dd + '-' + yyyy;
-    },
-    displayResult: function() {
-        var data = [
-            'Andalo',
-            'Monte Bondone'
-        ];
-        var html = [];
-        for(var i in data) {
-            // debugger;
-            html.push('<div class="uk-width-medium-1-2">');
-            html.push('<div class="uk-panel-box">');
-            html.push('<div class="uk-panel-badge">' + this._today + '</div>');
-            html.push('<h3 class="uk-text-center uk-h2">');
-            html.push(data[i]);
-            html.push('</h3>');
-            var item = this._result[data[i]];
-            for (var j in item) {
-                html.push('<img src="');
-                html.push(item[j].url);
-                html.push('" alt="' + item[j].name + '">');
-            }
-            html.push('</div>');
-            html.push('</div>');
-        }
-        this._div.append(html.join(''));
+    if (dd < 10) {
+      dd = `0${dd}`;
     }
-};
-var test = new Skicams();
-test.init();
+    if (mm < 10) {
+      mm = `0${mm}`;
+    }
+
+    this.today = `${mm}-${dd}-${yyyy}`;
+  }
+
+  displayResult() {
+    const data = ['Andalo', 'Monte Bondone'];
+    const html = [];
+    Object.keys(data).forEach(i => {
+      html.push('<div class="uk-width-medium-1-2">');
+      html.push('<div class="uk-panel-box">');
+      html.push(`<div class="uk-panel-badge">${this.today}</div>`);
+      html.push('<h3 class="uk-text-center uk-h2">');
+      html.push(data[i]);
+      html.push('</h3>');
+      const item = this.result[data[i]];
+      Object.keys(item).forEach(j => {
+        html.push('<img src="');
+        html.push(item[j].url);
+        html.push(`" alt="${item[j].name}">`);
+      });
+      html.push('</div>');
+      html.push('</div>');
+    });
+    this.div.append(html.join(''));
+  }
+}
+
+export default Skicams;
